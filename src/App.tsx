717b0f7d314c987cada7b0cdfea16860bf0bd0e0
Rishi Bhalla <rishi@illogical.in>
2025-10-26 11:43:22 +0530

    fix: fixed the router and scroll issue
    Issue description: When you scroll down on the home page (or any page)
    then click some link that changes the url location (using
    react-router-dom's Link element) the scroll gets stuck on that position
    instead of going to the top of the new page. This created a bad UX.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import Team from "./pages/Team";
 import Services from "./pages/Services";
@@ -13,8 +13,18 @@ import Careers from "./pages/Careers";
 import ThankYou from "./pages/ThankYou";
 import Portfolio from "./pages/Portfolio";
 import NotFound from "./pages/NotFound";
+import { useLayoutEffect } from "react";
 
 const queryClient = new QueryClient();
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }, [location.pathname]);
+
+  return children;
+}
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -22,17 +32,19 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/careers" element={<Careers />} />
-          <Route path="/thank-you" element={<ThankYou />} />
-<Route path="/portfolio" element={<Portfolio />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Wrapper>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/thank-you" element={<ThankYou />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Wrapper>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
